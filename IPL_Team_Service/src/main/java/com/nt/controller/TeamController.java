6e8dff8cf3282bce6bc2a56cc42f50947c0e8baa
Satyam Kumar <your-email@example.com>
2026-05-21 12:04:56 +0530

    TeamMs commit

---
+++ IPL_Team_Service/src/main/java/com/nt/controller/TeamController.java
@@ -0,0 +1,18 @@
+package com.nt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/team")
+public class TeamController {
+
+	@GetMapping("/test")
+	public String testing() {
+		return new String("GetMapping calling : Application Running");
+	}
+
+	
+
+}
